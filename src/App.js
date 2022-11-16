8e0288726ee89d7f92d24e4f5764bccd81c64a3e
LAPTOP-V7RKI3PV\toanh <toan.hoang@team.enouvo.com>
2022-11-16 15:55:59 +0700

    no message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React Deploy ECS
+          Hello Ban Thuong
         </a>
       </header>
     </div>
